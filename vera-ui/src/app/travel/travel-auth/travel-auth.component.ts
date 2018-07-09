4779c8498d918b5f7e147344f1d27c88bb2be6f7
Bethany Alcamo <balcamo@verawaterandpower.com>
2018-07-09 10:08:03 -0700

    Finished checking required fields

--- vera-ui/src/app/travel/travel-auth/travel-auth.component.ts
+++ vera-ui/src/app/travel/travel-auth/travel-auth.component.ts
@@ -19,7 +19,6 @@ export class TravelAuthComponent implements OnInit {
   eventLocBorder = "black";
   travelStartBorder = "blcak";
   travelEndBorder = "blcak";
-  districtVehicalBorder = "black";
   registrationBorder = "black";
   airfareBorder = "black";
   rentalBorder = "black";
@@ -29,7 +28,9 @@ export class TravelAuthComponent implements OnInit {
   perDiemBorder = "black";
   daysBorder = "black";
   miscBorder = "black";
-
+  advanceColor = "black";
+  distVehColor = "black";
+  policyColor = "black";
   constructor(http: Http) {
     this.http = http;
   }
@@ -49,72 +50,54 @@ export class TravelAuthComponent implements OnInit {
     console.log(this.form);
 
     if (this.checkRequired()) {
-      alert("success");
+      //this.http.get('bigfoot.verawp.local/api/API')
+      //.subscribe((data) => alert(data));
       this.setFormDefaults();
       this.nameBorder, this.phoneBorder = "black";
     } else {
       alert("Please fill in the required fields");
     }
-    //this.http.get('/api/API')
-      //.subscribe((data) => console.log(data));
+    
   }
 
   checkRequired() {
     var valid = true;
-    this.nameBorder = (this.form.Name == null ? "red" : "black");
-    if (this.form.Phone == null) {
-      this.phoneBorder = "red";
-      valid = false;
-    } if (this.form.EventTitle == null) {
-      this.eventTitleBorder = "red";
-      valid = false;
-    } if (this.form.Location == null) {
-      this.eventLocBorder = "red";
-      valid = false;
-    } if (this.form.TravelBegin == null) {
-      this.travelStartBorder = "red";
-      valid = false;
-    } if (this.form.TravelEnd == null) {
-      this.travelEndBorder = "red";
-      valid = false;
-    } if (this.form.DistVehicle == null) {
-      this.districtVehicalBorder = "red";
-      valid = false;
-    } if (this.form.RegistrationCost == null) {
-      this.registrationBorder = "red";
-      valid = false;
-    } if (this.form.Airfare == null) {
-      this.airfareBorder = "red";
-      valid = false;
-    } if (this.form.RentalCar == null) {
-      this.rentalBorder = "red";
-      valid = false;
-    } if (this.form.FuelParking == null) {
-      this.fuelBorder = "red";
-      valid = false;
-    } if (this.form.Mileage == null) {
-      this.mileageBorder = "red";
-      valid = false;
-    } if (this.form.Lodging == null) {
-      this.lodgingBorder = "red";
-      valid = false;
-    } if (this.form.PerDiem == null) {
-      this.perDiemBorder = "red";
-      valid = false;
-    } if (this.form.FullDays == null) {
-      this.daysBorder = "red";
-      valid = false;
-    } if (this.form.Misc == null) {
-      this.miscBorder = "red";
+    if (this.form.Name == null || this.form.Phone == null || this.form.EventTitle == null
+      || this.form.Location == null || this.form.TravelBegin == null || this.form.FullDays == null
+      || this.form.Mileage == null || this.form.Lodging == null || this.form.PerDiem == null
+      || this.form.RentalCar == null || this.form.FuelParking == null || this.form.Misc == null
+      || this.form.TravelEnd == null || this.form.DistVehicle == null || this.form.Airfare == null
+      || this.form.RegistrationCost == null || this.form.Advance == null || this.form.DistVehicle == null
+      || this.form.Policy == null
+      ) {
       valid = false;
     }
+
+    this.nameBorder = (this.form.Name == null ? "red" : "black");
+    this.phoneBorder = (this.form.Phone == null ? "red" : "black");
+    this.eventTitleBorder = (this.form.EventTitle == null ? "red" : "black");
+    this.eventLocBorder = (this.form.Location == null ? "red" : "black");
+    this.travelStartBorder = (this.form.TravelBegin == null ? "red" : "black");
+    this.travelEndBorder = (this.form.TravelEnd == null ? "red" : "black");
+    this.distVehColor = (this.form.DistVehicle == null ? "red" : "black");
+    this.registrationBorder = (this.form.RegistrationCost == null ? "red" : "black");
+    this.airfareBorder = (this.form.Airfare == null ? "red" : "black");
+    this.rentalBorder = (this.form.RentalCar == null ? "red" : "black");
+    this.fuelBorder = (this.form.FuelParking == null ? "red" : "black");
+    this.mileageBorder = (this.form.Mileage == null ? "red" : "black");
+    this.lodgingBorder = (this.form.Lodging == null ? "red" : "black");
+    this.perDiemBorder = (this.form.PerDiem == null ? "red" : "black");
+    this.daysBorder = (this.form.FullDays == null ? "red" : "black");
+    this.miscBorder = (this.form.Misc == null ? "red" : "black");
+    this.advanceColor = (this.form.Advance == null ? "red" : "black");
+    this.policyColor = (this.form.Policy == null ? "red" : "black");
+
     return valid;
 
   }
   setFormDefaults() {
     this.form.Name = null;
     this.form.Phone = null;
-
     this.form.Email = null;
     this.form.EventTitle = null;
     this.form.Location = null;
@@ -134,7 +117,5 @@ export class TravelAuthComponent implements OnInit {
     this.form.total = null;
     this.form.Advance = null;
     this.form.Policy = null;
-
-
   }
 }
