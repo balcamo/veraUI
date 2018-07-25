2afa477d7bdfda24ad31c7fc0165b5c7860899b3
veratroy <41026859+veratroy@users.noreply.github.com>
2018-07-25 13:37:37 -0700

    Included Data Handler and code for SubmitForm

--- veraAPI/veraAPI/HelperClasses/FormHelp.cs
+++ veraAPI/veraAPI/HelperClasses/FormHelp.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using veraAPI.Models;
 using veraAPI.Models.Forms;
 
 namespace veraAPI.HelperClasses
 {
     public class FormHelp
     {
+        public UIDataHandler UIData = new UIDataHandler("Azure", "Valhalla");
+        
         public void SubmitForm(BaseForm form, int templateID)
         {
-            // JobHeader Job = new JobHeader();
-            // TODO : Impliment get to query template table
-
-            // TODO : Impliment Data Object Insert, catch ID
-            // TODO : Impliment Job Header Insert
-
-            // TODO : impliment method to call email service
-
+            UIData.FormData = FormData;
+            if (UIData.LoadJobTemplate(UIData.FormData.TemplateID))
+            {
+                if (UIData.InsertFormData())
+                    UIData.InsertJob();
+            }
+            
             return;
         }
     }
