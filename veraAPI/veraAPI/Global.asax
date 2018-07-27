ac2d72d420849239f822a828c020aa3ac415c125
Bethany Alcamo <balcamo@verawaterandpower.com>
2018-07-27 11:25:25 -0700

    fixing namespace

--- veraAPI/veraAPI/Global.asax
+++ veraAPI/veraAPI/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="veraAPI.WebApiApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VeraAPI.WebApiApplication" Language="C#" %>
--- veraAPI/veraAPI/Properties/AssemblyInfo.cs
+++ veraAPI/veraAPI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("veraAPI")]
+[assembly: AssemblyTitle("VeraAPI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("veraAPI")]
+[assembly: AssemblyProduct("VeraAPI")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
