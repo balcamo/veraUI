3439f159bc94f820896c04cf64ed85642700d9fc
Bethany Alcamo <balcamo@verawaterandpower.com>
2018-08-10 14:57:24 -0700

    display auth form information

--- vera-ui/src/app/travel/view-auth-forms/view-auth-forms.component.ts
+++ vera-ui/src/app/travel/view-auth-forms/view-auth-forms.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { UserService } from '../../service/app.service.user';
 import { User } from '../../classes/user';
 import { Headers, Http, URLSearchParams, RequestOptions } from '@angular/http';
+import { AuthForm } from '../../classes/travel-auth-form';
 
 @Component({
   selector: 'app-view-auth-forms',
@@ -9,10 +10,16 @@ import { Headers, Http, URLSearchParams, RequestOptions } from '@angular/http';
   styleUrls: ['./view-auth-forms.component.scss']
 })
 export class ViewAuthFormsComponent implements OnInit {
+  //@Input() authForms;
   http: Http;
   userService: UserService
   user: User;
-
+  displayForm = "none";
+  //temp
+  authForms = [];
+  form: AuthForm;
+  auth1 = new AuthForm();
+  auth2 = new AuthForm();
   constructor(http: Http, userService: UserService) {
     this.http = http;
     this.userService = userService;
@@ -20,6 +27,19 @@ export class ViewAuthFormsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.auth1.EventTitle = "Event 1";
+    this.auth2.EventTitle = "Event 2";
+    this.authForms.push(this.auth1);
+    this.authForms.push(this.auth2);
+  }
+  displaySelected(authForm: AuthForm) {
+    this.form = authForm;
+    console.log(this.form);
+    if (this.displayForm == "none") {
+      this.displayForm = "block";
+    } else {
+      this.displayForm = "none"
+    }
   }
 
 }
