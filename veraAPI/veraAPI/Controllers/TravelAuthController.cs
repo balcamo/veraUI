5887c1f0833c3921946c9ecbabb1c85f8a588539
Troy Huff <thuff@verawaterandpower.com>
2018-08-21 09:54:52 -0700

    increased logging and corrected return from email helper

--- veraAPI/veraAPI/Controllers/TravelAuthController.cs
+++ veraAPI/veraAPI/Controllers/TravelAuthController.cs
@@ -42,28 +42,36 @@ namespace VeraAPI.Controllers
         // POST: api/API
         public string Post([FromBody]TravelAuthForm value)
         {
+            Log.WriteLogEntry("Begin Post TravelAuthForm...");
             string result = string.Empty;
             value.TemplateID = TemplateIndex.InsertTravelAuth;
             try
             {
                 if (value.GetType() == typeof(TravelAuthForm))
                 {
+                    Log.WriteLogEntry("Verify submitted form is the correct type.");
                     value.setNulls();
 
                     Task t = Task.Run(() =>
                     {
+                        Log.WriteLogEntry("Start Task to submit the travel form.");
                         helper = new FormHelp();
                         // change number to constant once file is made
                         if (helper.SubmitForm(value))
                         {
+                            Log.WriteLogEntry("Success submitting travel form.");
+                            Log.WriteLogEntry("Start EmailHelper.");
                             TravelEmail = new EmailHelper();
+                            Log.WriteLogEntry("Call Travel email helper load user with user email " + helper.userEmail);
                             if (TravelEmail.LoadUser(helper.userEmail))
                             {
-                                TravelEmail.SendEmail();
+                                Log.WriteLogEntry("Success loading email user data.");
+                                if (TravelEmail.SendEmail())
+                                    Log.WriteLogEntry("Success sending travel authorization email to department head.");
                             }
-                            result = "Travel Authorization Form Submitted.";
                         }
                     });
+                    result = "Travel Authorization Form Submitted.";
                     //Thread helpThread = new Thread(authHelper.SubmitAuthForm);
 
                     //helpThread.Start(authForm);
