371fcd2d866e135e4e16b87caba40eec08ce53a6
Troy Huff <thuff@verawaterandpower.com>
2018-12-03 08:10:27 -0800

    updated web.config

---
+++ veraAPI/veraAPI/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Host.SystemWeb;
+using Owin;
+
+[assembly: OwinStartup(typeof(VeraAPI.Startup))]
+
+namespace VeraAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
